efac51e3f4d0d936f83fa1fa1997240a44e820a4
Hoyeung <hoyeungw@outlook.com>
2021-12-03 10:07:15 +0800

    renamed columns mapper to map_columns

--- src/columns/mapper.rs
+++ src/columns/mapper.rs
@@ -4,11 +4,11 @@ use crate::vector::mapper as mapper_vector;
 pub trait Mappers<R>: IntoIterator<Item=R>
     where R: IntoIterator
 {
-    fn mapper<P, F>(self, mut f: F) -> Vec<P> where
+    fn map_columns<P, F>(self, mut fn_on_column: F) -> Vec<P> where
         Self: Sized,
         R::IntoIter: ExactSizeIterator<Item=R::Item>,
         F: FnMut(Vec<R::Item>) -> P
-    { mapper_vector(self.transpose(), |col| f(col)) }
+    { mapper_vector(self.transpose(), |col| fn_on_column(col)) }
 }
 
 impl<R, M> Mappers<R> for M where
@@ -16,12 +16,20 @@ impl<R, M> Mappers<R> for M where
     R: IntoIterator
 {}
 
+pub fn map_columns<R, M, F, P>(matrix: M, f: F) -> Vec<P> where
+    M: IntoIterator<Item=R>,
+    R: IntoIterator,
+    R::IntoIter: ExactSizeIterator<Item=R::Item>,
+    F: FnMut(Vec<R::Item>) -> P
+{ matrix.map_columns(f) }
+
+#[deprecated(since = "0.0.20", note = "please use `map_columns` instead")]
 pub fn mapper<R, M, F, P>(matrix: M, f: F) -> Vec<P> where
     M: IntoIterator<Item=R>,
     R: IntoIterator,
     R::IntoIter: ExactSizeIterator<Item=R::Item>,
     F: FnMut(Vec<R::Item>) -> P
-{ matrix.mapper(f) }
+{ matrix.map_columns(f) }
 
 #[cfg(test)]
 mod tests {
@@ -30,7 +38,7 @@ mod tests {
     #[test]
     fn test() {
         let matrix = vec![vec![1, 2, 3], vec![1, 2, 3], vec![1, 2, 3]];
-        let result = (&matrix).mapper(|col| mapper_vector(col, |x| { x + 1 }));
+        let result = (&matrix).map_columns(|col| mapper_vector(col, |x| { x + 1 }));
         println!(">> result = {:?}", result);
         println!(">> matrix = {:?}", matrix);
     }
